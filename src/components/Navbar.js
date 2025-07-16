a9d31a64616e4c2375b6ba41b3019447ba2f3e8a
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-15 22:44:43 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import './Navbar.css';
 
+const Navbar = () => (
+  <header>
+    <nav>
+      <div className="logo">Sridevi Velpula</div>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#resume">Resume</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navbar;
