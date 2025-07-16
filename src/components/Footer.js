13ed3d5fe3af0e377a18a6c1444156164a323712
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-16 16:34:43 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-400 text-center py-6 text-sm">
+    <footer className="bg-tealcustom border-tealcustom text-center py-6 text-sm">
       © {new Date().getFullYear()} Sridevi Velpula. All rights reserved.
     </footer>
   );
