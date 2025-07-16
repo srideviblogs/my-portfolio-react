63dd5061a359e98ff5d272699347dbfed43d5419
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-15 22:45:21 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,11 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>I’m Sridevi Velpula, a DevOps Engineer with 6+ years of experience in IT. I’ve worked for companies like Virtusa and Infosys, supporting major clients such as Citi Bank, PwC, and Conduent. I'm passionate about automating infrastructure, optimizing CI/CD pipelines, and working with cloud-native technologies like Kubernetes and Terraform.</p>
+  </section>
+);
+
+export default About;
 
