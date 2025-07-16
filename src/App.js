c4e3253639dfaf890550c1b0028b4ea1efa20c5f
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-15 22:43:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
