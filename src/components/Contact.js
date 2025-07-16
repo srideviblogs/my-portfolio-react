aa9a3a7ac4ec2d79bd4858ffbfc13d0364ef7a3a
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-15 22:46:24 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <p>Email: <a href="mailto:sridevi.velpula.official@gmail.com">sridevi.velpula.official@gmail.com</a></p>
+    <p>GitHub: <a href="https://github.com/srideviblogs/" target="_blank">github.com/srideviblogs</a></p>
+    <p>Blog: <a href="https://srideviblogs.github.io" target="_blank">srideviblogs.github.io</a></p>
+  </section>
+);
+
+export default Contact;
 
