2bb7a87c5b20021564ffffe083c8a82f3462cef7
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-16 16:28:41 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full z-20 bg-tealcustom text-white shadow-lg">
+    <nav className="fixed w-full z-20 bg-tealcustom text-white shadow-lg h-20 flex items-center">
       <div className="container mx-auto flex justify-between items-center p-4">
         <h1 className="text-2xl font-bold">Sridevi Velpula</h1>
 
