d7bd9d2096249e7688238f937ebdce1536df19ed
srideviblogs <84090678+srideviblogs@users.noreply.github.com>
2025-07-17 13:51:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Resume from './components/Resume';
+import Expertise from "./components/Expertise";
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
